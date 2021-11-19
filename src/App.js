5cd8ae3184b07f19b66283e14af760138c6842ed
emmaEzelisiaku <offixcialballer@gmail.com>
2021-11-19 08:50:15 -0800

    just made changes to message styling

--- src/App.js
+++ src/App.js
@@ -1,88 +1,45 @@
-import { useState,useRef } from "react";
-import firebase from "firebase/compat/app";
-import "firebase/compat/firestore";
-import "firebase/compat/auth";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollectionData} from "react-firebase-hooks/firestore";
-import './App.css'
-firebase.initializeApp({
-  apiKey: "AIzaSyDFIZc-K5S7Oq96RWrLZTsiN3FLw0nWvNU",
-  authDomain: "whatsapp-498c9.firebaseapp.com",
-  projectId: "whatsapp-498c9",
-  storageBucket: "whatsapp-498c9.appspot.com",
-  messagingSenderId: "484480416848",
-  appId: "1:484480416848:web:7db30b7a91d1f76f20a555",
-});
-const auth = firebase.auth();
-const firestore = firebase.firestore();
-function App() {
+// import React from 'react'
+// import Message from './pages/Messgage'
+// import HomePage from './pages/HomePage'
 
-  const [user] = useAuthState(auth);
+// import { Switch, Route, Redirect } from 'react-router-dom';
+// const App = () => {
+//   return (
+//     <div>
+//         <Switch>
+//           <Route exact path='/' component={HomePage} />
+//           <Route path='/message' component={Message} />
+//           <Route
+//             exact
+//             path='/signin'
+//             render={() =>
+//               this.props.currentUser ? (
+//                 <Redirect to='/' />
+//               ) : (
+//                 <SignInAndSignUpPage />
+//               )
+//             }
+//           />
+//         </Switch>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React from 'react'
+
+import Message from './pages/Messgage'
+import HomePage from './pages/HomePage'
+
+const App = () => {
   return (
-    <div className="App">
-      <header></header>
-      <section>{user ? <ChatRoom /> : <SignIn />}</section>
+    <div>
+      <Message/>
+      {/* <HomePage/> */}
     </div>
-  );
+  )
 }
-function SignIn() {
-  const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-  };
-  return <button onClick={signInWithGoogle}>sign in with google</button>;
-}
-function SignOut() {
-  return (
-    auth.currentUser && <button onClick={() => auth.signOut()}>sign out</button>
-  );
-}
-function ChatRoom() {
 
-  const dummy = useRef()
-  const messagesRef = firestore.collection("messages");
-  const query = messagesRef.orderBy("createdAt").limit(25);
-  const [messages] = useCollectionData(query, { idField: "id" });
-
-  const [formValue, setFormValue] = useState("");
-  const sendMessage = async(e) => {
-    e.preventDefault();
-    const { uid, photoURL } = auth.currentUser;
-    await messagesRef.add({
-      text: formValue,
-      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      uid,
-      photoURL,
-    });
-    setFormValue("");
-
-    dummy.current.scrollIntoView({behaviour:'smooth'});
-  };
-  return (
-    <>
-      <main>
-        {messages &&
-          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
-      <div ref={dummy}></div>
-      </main>
-      <form onSubmit={sendMessage}>
-        <input
-          value={formValue}
-          onChange={(e) => setFormValue(e.target.value)}
-        />
-        <button type="submit">send</button>
-      </form>
-    </>
-  );
-}
-function ChatMessage(props) {
-  const { text, uid, photoURL } = props.message;
-  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
-  return (
-    <div className={`message ${messageClass}`}>
-      <img src={photoURL} />
-      <p>{text}</p>
-    </div>
-  );
-}
-export default App;
+export default App
